78b78543d75c9a8a44b61a486feb5daa5ecce174
Márton Herdey <47821828+herdeym@users.noreply.github.com>
2021-11-22 21:44:02 +0100

    topwindow

--- ScreenMate/Controller/Components/SitOnWindowComponent.cs
+++ ScreenMate/Controller/Components/SitOnWindowComponent.cs
@@ -13,6 +13,11 @@ namespace ScreenMate.Controller.Components
     {
         [DllImport("user32.dll")]
         private static extern int GetWindowRect(IntPtr hwnd, out Rectangle rect);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetTopWindow(IntPtr hwnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
         private Rectangle position;
 
         public override void RunComponent()
@@ -35,6 +40,9 @@ namespace ScreenMate.Controller.Components
             var selectedWindow = openWindowProcesses.ElementAt(random.Next(openWindowProcesses.Count()));
             Debug.WriteLine(selectedWindow.ProcessName);
             var windowHandler = selectedWindow.MainWindowHandle;
+            var topW = GetTopWindow(IntPtr.Zero);
+            windowHandler = GetWindow(topW, 2);
+            windowHandler = GetWindow(windowHandler, 2);
 
             GetWindowRect(windowHandler, out position);
 
